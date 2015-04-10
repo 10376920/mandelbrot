9a553e953b96ca2ba966c4047a3c61881e2a43a1
Clelia Bambini <clelia.bambini@mail.polimi.it>
2015-04-10 18:52:08 +0200

    Creata la griglia complessa e valutata la successione nei punti.

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,52 @@
 using complex= std::complex<double>;
 
 int main () {
+	
+	double n=100,m=100;
+	double x0=-1 , x=-1, y=1;
+	double h=(y-x)/(n-1), k=(y-x)/(m-1);  //considero la griglia quadrata o non me la cavo più
+	
+	// definisco la matrice in cui inserire i numeri complessi
+	Matrix<complex> mat(n,m);
 
-	Matrix<int> mat(2,2);
-	mat(1,1)=1;
-	mat(1,2)=2;
-	mat(2,1)=3;
-	mat(2,2)=4;
-	
-	std::cout << mat;
+	//nella matrice voglio inserire i numeri complessi compresi in una porzione di piano di Gauss tra -1 e 1 e -i e i,
+	// mantenendo l'ordine della griglia
+	for (int i=1; i<=n; i++){
+		x=x0;
+		for (int j=1; j<=m; j++) {
+			mat(i,j)=complex(x,y);
+			x=x+h;
+		}
+		y=y-k;
+	}
 		
-	Matrix<complex> mat1(2,2);
-	mat1(1,1)=complex(1,1);
-	mat1(1,2)=complex(1,2);
-	mat1(2,1)=complex(1,3);
-	mat1(2,2)=complex(1,4);
+	std::cout << mat;
 	
-	std::cout << mat1;
+	//su ogni punto della matrice valuto la sottosuccessione per N iterazioni
+	// se ha modulo maggiore di due diverge, non appartiene all'insieme, è interessante e mi salvo quanto ci ho messo ad arrivare lì
+	//se alla fine ha modulo minore di due converge quidi non è un punto interessante
 	
+	int N=100;
+	Matrix<int> Iteraz(n,m);
 	
-	return 0;
+	for (int i=1; i<=n; i++){
+		for (int j=1; j<=m; j++) {
+			complex z(0,0);	
+			
+			for (int it=0; it < N; ++it){
+				if (std::norm(z) > 2 ) {
+					Iteraz(i,j)=it;
+					break; //se è maggiore di due bona, diverge, salva e esci dal ciclo
+					}
+					
+				z=z*z+mat(i,j);
+			}
+			if (std::norm(z)<2)
+				Iteraz(i,j)=0; //se converge salvo arbitrariamente 0 come valore.
+		}
+	}
+	
+	std::cout << Iteraz;
+	
+return 0;
 }
