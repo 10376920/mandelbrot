182a8399fe6d51a1d2a6d5efded89b166c3b0b27
Luca Giussani <luca3.giussani@mail.polimi.it>
2015-04-13 10:10:25 +0200

    Added header guard to the Matrix.hpp file

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,3 +1,6 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
 #include <vector>
 #include <iostream>
 
@@ -53,3 +56,5 @@ std::ostream & operator<< (std::ostream & os, const Matrix<T> & mat ){
 
 	return os;
 }
+
+#endif
